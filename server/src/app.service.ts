699a4c407c739c71febdd2cf1159d9dfe1bd6220
Luka Matić <luka.mat99@gmail.com>
2021-12-20 05:05:32 +0100

    Update app.service.ts

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Hello, I'm deployed to heroku yaaayy! ^__^";
+    return 'Hello! ^__^';
   }
 }
