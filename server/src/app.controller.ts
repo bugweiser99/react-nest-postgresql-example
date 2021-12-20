0220d6e5664372b102c645a585becbce8f814a2a
Luka Matić <luka.mat99@gmail.com>
2021-12-20 06:38:09 +0100

    implemented file upload

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller('api/app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', { dest: './uploads' }))
+  uploadFile(): string {
+    return 'file uploaded';
   }
 }
--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,18 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
   const [text, setText] = useState('');
 
+  const fileInput = useRef<HTMLInputElement>(null);
+
   useEffect(() => {
     const fetchText = async () => {
-      const response = await fetch('/api/app');
+      const response = await fetch('/api/app/hello');
       setText(await response.text());
     };
 
     fetchText();
   }, []);
 
-  return <div className='text-yellow-500 bg-purple-500'>{text}</div>;
+  const uploadFile = async () => {
+    const body = new FormData();
+    const file: File = fileInput.current!.files![0];
+    body.append('file', file);
+
+    const response = await fetch('/api/app/upload', {
+      method: 'post',
+      body: body,
+    });
+
+    if (response.status === 201) {
+      setText(await response.text());
+    } else {
+      setText('upload failed');
+    }
+  };
+
+  return (
+    <div className='ml-10'>
+      <div className='text-purple-500 mb-3'>{text}</div>
+      <div className='mb-3'>
+        <input type='file' ref={fileInput} />
+      </div>
+      <div>
+        <button
+          className='bg-purple-500 text-white px-2 py-1 rounded'
+          onClick={uploadFile}
+        >
+          Upload chosen file
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
